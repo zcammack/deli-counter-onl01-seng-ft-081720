f92fd9a56bffdb004fc9d478df92d53304993b67
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 15:48:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(deli, name)
   deli << name
-  puts "Welcome #{name}, you are number #{deli.length} in line."
+  puts "Welcome, #{name}, you are number #{deli.length} in line."
 end
 
 def now_serving()
