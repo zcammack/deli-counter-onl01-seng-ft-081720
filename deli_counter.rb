7c6b5133dadc2dd1320dcdfa76bfcc4dc46d8695
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 13:48:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line (katz_deli)
+  if katz_deli.length == 0
+    "The line is currently empty."
+  else
+    katz_deli.each.with_index do |customer, idx|
+      "#{customer} is number #{idx} in line."
+end
