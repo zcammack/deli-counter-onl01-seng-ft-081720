b5e4a19a19d0680f25692181b89c31501cb42635
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 14:08:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
     end
-  else katz_deli.each_with_index do |customer, idx|
+  elsif katz_deli.each_with_index do |customer, idx|
     deli_line << "#{idx + 1}. #{customer}"
   end
   puts "The line is currently: #{deli_line}"
